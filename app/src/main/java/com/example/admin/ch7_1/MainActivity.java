4b6e1114cfd7161b5df5a2e29a9a646813b7a165
chenyirou <annie851216@gmail.com>
2018-04-17 10:38:21 +0800

    測試長按事件

--- app/src/main/java/com/example/admin/ch7_1/MainActivity.java
+++ app/src/main/java/com/example/admin/ch7_1/MainActivity.java
@@ -2,12 +2,27 @@ package com.example.admin.ch7_1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, View.OnLongClickListener {
+    private TextView output;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        output = (TextView) findViewById(R.id.txv_output);
+        Button btn = (Button) findViewById(R.id.btn_longclick);
+        btn.setOnClickListener(this);
+        btn.setOnLongClickListener(this);
+    }
+    public boolean onLongClick(View v){
+        output.setText("觸發LongClick事件...");
+        return false; //到這裡還沒結束，還會繼續執行onClick事件
+        //return true;     //執行完onLongClick事件後就結束了，不會再繼續執行下面的事件，到此為止的意思
+    }
+    public void onClick(View v){
+        output.setText("觸發Click事件...");
     }
 }
